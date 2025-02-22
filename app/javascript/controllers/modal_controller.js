76fdd130225a7ab603567de3185ae10c50d90df4
Marc Best <marcbest123@gmail.com>
2025-02-22 12:23:16 +0000

    Add initial sharing modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,29 @@
+// app/javascript/controllers/modal_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["modal"]
+
+  connect() {
+    // Add event listener for escape key
+    document.addEventListener('keydown', this.handleKeydown.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('keydown', this.handleKeydown.bind(this))
+  }
+
+  open() {
+    this.modalTarget.classList.remove('hidden')
+  }
+
+  close() {
+    this.modalTarget.classList.add('hidden')
+  }
+
+  handleKeydown(event) {
+    if (event.key === 'Escape') {
+      this.close()
+    }
+  }
+}
